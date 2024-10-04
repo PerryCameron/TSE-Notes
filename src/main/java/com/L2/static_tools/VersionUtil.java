62fba9525909c162ef0b0d5b1c89d825af74cc67
Perry Cameron <pcameron28@yahoo.com>
2024-10-04 10:10:21 -0400

    Added version.properties file and static helper class to retrieve the version number

---
+++ src/main/java/com/L2/static_tools/VersionUtil.java
@@ -0,0 +1,39 @@
+package com.L2.static_tools;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class VersionUtil {
+
+    private static final String VERSION_FILE = "/version.properties";
+
+    public static String getVersion() {
+        Properties props = new Properties();
+        try (InputStream input = VersionUtil.class.getResourceAsStream(VERSION_FILE)) {
+            if (input == null) {
+                System.err.println("Unable to find version.properties file");
+                return "Unknown";
+            }
+            props.load(input);
+            String version = props.getProperty("version", "Unknown");
+
+            // Remove leading "v"
+            if (version.startsWith("v")) {
+                version = version.substring(1);
+            }
+
+            // Remove commit hash (-gxxxxxxx)
+            int commitHashIndex = version.indexOf("-g");
+            if (commitHashIndex != -1) {
+                version = version.substring(0, commitHashIndex);
+            }
+
+            return version;
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return "Unknown";
+        }
+    }
+}
+
