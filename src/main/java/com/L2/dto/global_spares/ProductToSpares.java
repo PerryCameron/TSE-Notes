30443559bd515e2af169a272e73f8f4e2da27007
Perry Cameron <pcameron28@yahoo.com>
2025-04-30 10:01:45 -0400

    added new fields to productToSparesDTO

--- src/main/java/com/L2/dto/global_spares/ProductToSpares.java
+++ src/main/java/com/L2/dto/global_spares/ProductToSpares.java
@@ -13,12 +13,14 @@ public class ProductToSpares {
     private String productEndOfServiceDate;
     private String lastUpdate;
     private String addedToCatalogue;
+    private Boolean archived;
+    private Boolean custom_add;
 
     // Default constructor
     public ProductToSpares() {
     }
 
-    public ProductToSpares(String pimRange, String pimProductFamily, String spareItem, String replacementItem, String standardExchangeItem, String spareDescription, String catalogueVersion, String productEndOfServiceDate, String lastUpdate, String addedToCatalogue) {
+    public ProductToSpares(String pimRange, String pimProductFamily, String spareItem, String replacementItem, String standardExchangeItem, String spareDescription, String catalogueVersion, String productEndOfServiceDate, String lastUpdate, String addedToCatalogue, Boolean archived, Boolean custom_add) {
         this.pimRange = pimRange;
         this.pimProductFamily = pimProductFamily;
         this.spareItem = spareItem;
@@ -29,6 +31,8 @@ public class ProductToSpares {
         this.productEndOfServiceDate = productEndOfServiceDate;
         this.lastUpdate = lastUpdate;
         this.addedToCatalogue = addedToCatalogue;
+        this.archived = archived;
+        this.custom_add = custom_add;
     }
 
     // Getters and Setters
@@ -110,5 +114,21 @@ public class ProductToSpares {
 
     public void setAddedToCatalogue(String addedToCatalogue) {
         this.addedToCatalogue = addedToCatalogue;
+    }
+
+    public Boolean getArchived() {
+        return archived;
+    }
+
+    public void setArchived(Boolean archived) {
+        this.archived = archived;
+    }
+
+    public Boolean getCustom_add() {
+        return custom_add;
+    }
+
+    public void setCustom_add(Boolean custom_add) {
+        this.custom_add = custom_add;
     }
 }
