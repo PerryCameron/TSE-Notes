242747c43b07cb1b9e42ea538eba0a16dd060566
Perry Cameron <pcameron28@yahoo.com>
2025-06-26 10:10:02 -0400

    Added ImageResources class to make better use of RAM with images

--- src/main/java/com/L2/static_tools/ImageResources.java
+++ src/main/java/com/L2/static_tools/ImageResources.java
@@ -1,4 +1,8 @@
 package com.L2.static_tools;
 
+import javafx.scene.image.Image;
+
 public class ImageResources {
+    public static final Image YES_IMAGE = new Image(ImageResources.class.getResourceAsStream("/images/yes-16.png"));
+    public static final Image NO_IMAGE = new Image(ImageResources.class.getResourceAsStream("/images/no-16.png"));
 }
